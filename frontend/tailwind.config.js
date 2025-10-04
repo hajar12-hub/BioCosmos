48ede074283616b0670120b54619bfb40c000a1d
hajar12-hub <azaouhajar@gmail.com>
2025-10-04 08:14:19 +0100

    Add frontend, backend and README

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,75 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // tous les fichiers React dans src
+  ],
+  darkMode: ["class", "class"], // active le dark mode via .dark
+  theme: {
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			'card-foreground': 'var(--card-foreground)',
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			'popover-foreground': 'var(--popover-foreground)',
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			'primary-foreground': 'var(--primary-foreground)',
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			'secondary-foreground': 'var(--secondary-foreground)',
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			'muted-foreground': 'var(--muted-foreground)',
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			'accent-foreground': 'var(--accent-foreground)',
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			'destructive-foreground': 'var(--destructive-foreground)',
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			sidebar: 'var(--sidebar)',
+  			'sidebar-foreground': 'var(--sidebar-foreground)',
+  			'sidebar-primary': 'var(--sidebar-primary)',
+  			'sidebar-primary-foreground': 'var(--sidebar-primary-foreground)',
+  			'sidebar-accent': 'var(--sidebar-accent)',
+  			'sidebar-accent-foreground': 'var(--sidebar-accent-foreground)',
+  			'sidebar-border': 'var(--sidebar-border)',
+  			'sidebar-ring': 'var(--sidebar-ring)',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
+  },
+    plugins: [require("tailwindcss-animate")]
+}
